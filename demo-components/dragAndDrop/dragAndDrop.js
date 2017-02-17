aa5f122c468e7129792953ea5bb8682473604105
Toby Nilsen <torbjorn.nilsen@gmail.com>
2017-02-17 19:49:42 +0800

    Drag and drop. Improved.

--- demo-components/dragAndDrop/dragAndDrop.js
+++ demo-components/dragAndDrop/dragAndDrop.js
@@ -3,22 +3,40 @@ class DragAndDrop extends justright.Component{
 	constructor(){
 		super(justright.templates.get("DragAndDrop"));
 		justright.events.listen("drag",this,this.drag);
-		justright.events.listen("drop",this,this.drop);
 		justright.events.listen("mousePos",this,this.mousePos);
-		this._clickedX = 0;
-		this._clickedY = 0;
+		this._mousePosX = 0;
+		this._mousePosY = 0;
 	}
 	
 	mousePos(event){
 	    this._mousePosX = event.offsetX;
 	    this._mousePosY = event.offsetY;
+	    return false;
 	}
 	
-	drop(event){
+	drag(event){
 	    var container = this.get("container").getMappedElement();
 	    var mappedElement = this.get("message").getMappedElement();
-	    mappedElement.style.top = event.clientY - container.getBoundingClientRect().top - this._mousePosY;
-	    mappedElement.style.left = event.clientX - container.getBoundingClientRect().left - this._mousePosX;
+	    var topPos = event.clientY - container.getBoundingClientRect().top - this._mousePosY;
+	    var leftPos = event.clientX - container.getBoundingClientRect().left - this._mousePosX;
+	    var topMax = container.getBoundingClientRect().height - mappedElement.getBoundingClientRect().height;
+	    var leftMax = container.getBoundingClientRect().width - mappedElement.getBoundingClientRect().width;
+	    var topMin = 0;
+	    var leftMin = 0;
+	    if(topPos < topMin){
+	    	topPos = topMin;
+	    }
+	    if(leftPos < leftMin){
+	    	leftPos = leftMin;
+	    }
+	    if(topPos > topMax){
+	    	topPos = topMax;
+	    }
+	    if(leftPos > leftMax){
+	    	leftPos = leftMax;
+	    }
+	    mappedElement.style.top = topPos;
+	    mappedElement.style.left = leftPos;
 	}
 	   
 }
