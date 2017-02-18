2f869911d4b3a5214b25a083144e939d19af5e5f
Toby Nilsen <torbjorn.nilsen@gmail.com>
2017-02-19 03:17:25 +0800

    Improved drag and drop.

--- demo-components/dragAndDrop/dragAndDrop.js
+++ demo-components/dragAndDrop/dragAndDrop.js
@@ -3,32 +3,35 @@ class DragAndDrop extends justright.Component{
 	constructor(){
 		super(justright.templates.get("DragAndDrop"));
 		justright.events.listen("drag",this,this.drag);
-		justright.events.listen("mousePos",this,this.mousePos);
+		justright.events.listen("target",this,this.target);
+		justright.events.listen("flash",this,this.flash);
 		justright.events.listen("dragOver",this,this.dragOver);
-		this._mousePosX = 0;
-		this._mousePosY = 0;
+		this._target = null;
 	}
 	
 	dragOver(event){
 		event.preventDefault();
 	}
 	
-	mousePos(event){
-	    this._mousePosX = event.offsetX;
-	    this._mousePosY = event.offsetY;
+	flash(event){
+	    this.get("box").getStyleAttribute().set("background-color","red");
+	}
+	
+	target(event){
+	    this._target = event.getTarget();
 	    return false;
 	}
 	
 	drag(event){
-	    var container = this.get("container").getMappedElement();
-	    var mappedElement = this.get("message").getMappedElement();
-	    var topPos = event.clientY - container.getBoundingClientRect().top - this._mousePosY;
-	    var leftPos = event.clientX - container.getBoundingClientRect().left - this._mousePosX;
-	    var topMax = container.getBoundingClientRect().height - mappedElement.getBoundingClientRect().height;
-	    var leftMax = container.getBoundingClientRect().width - mappedElement.getBoundingClientRect().width;
+	    var container = this.get("container");
+	    var box = this.get("box");
+	    var topPos = event.getY() - container.getTop() - this._target.getY();
+	    var leftPos = event.getX() - container.getLeft() - this._target.getX();
+	    var topMax = container.getHeight() - box.getHeight();
+	    var leftMax = container.getWidth() - box.getWidth();
 	    var topMin = 0;
 	    var leftMin = 0;
-	    if(event.clientY === 0 || event.clientX === 0){
+	    if(event.getY() === 0 || event.getX() === 0){
 	        return;
 	    }
 	    if(topPos < topMin){
@@ -43,8 +46,8 @@ class DragAndDrop extends justright.Component{
 	    if(leftPos > leftMax){
 	    	leftPos = leftMax;
 	    }
-	    mappedElement.style.top = topPos;
-	    mappedElement.style.left = leftPos;
+	    box.getStyleAttribute().set("top",topPos);
+	    box.getStyleAttribute().set("left",leftPos);
 	}
 	   
 }
